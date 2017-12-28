039e3b3963abdbb85887e4c33a30c25b75f54c0e
Steffen Netz <steffennetz@posteo.de>
2017-12-28 18:35:31 +0100

    QIF.cs eingefügt

---
+++ PDFExtract/QIF.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFExtract
+{
+    class QIF
+    {
+
+//         Letter Definition
+// D Date(optional)
+// N Action
+// Y Security
+// I Price
+// Q Quantity(# of shares or split ratio) 
+// C Cleared status
+// P  1st line text for transfers/reminders
+// M  Memo
+// O  Commission
+// L  Account for transfer
+// T  Amount of transaction
+// U  Amount of transaction (higher possible value than T)
+// $  Amount transferred 
+// ^  End of entry
+        //!Type:Invst
+        //D8.1.17
+        //V8.3.17
+        //NKauf
+        //FEUR
+        //G1.000000
+        //MWERTPAPIERE COBA ZT.EST50P UNLTD
+        //YCOBA ZT.EST50P UNLTD
+        //~726431
+        //@DE0007264319
+        //&8
+        //I100
+        //Q1.000000
+        //U100.00
+        //O0.00|0.00|0.00|0.00|0.00|0.00|0.00|0.00|0.00|0.00|0.00|0.00
+        //L|[comdirect Steffen]
+        //$100.00
+        //B0.00|0.00|0.00
+        //^
+
+
+
+    }
+}
